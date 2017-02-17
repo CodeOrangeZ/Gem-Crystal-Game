cd58e5ba03bf5d9a0b8205b36097d12abeed6873
Zackery Rilling <Rilling@Zackerys-MacBook-Pro.local>
2017-02-16 16:29:45 -0800

    updated to be a object game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,132 @@
+// updated to meet requirements of object
+var game = {
+	userScore: 0,
+	winsCount: 0,
+	losesCount: 0,
+	rubyValue: null,
+	emeraldValue: null,
+	sapphireValue: null,
+	diamondValue: null,
+	crystals: ["ruby", "emerald", "sapphire", "diamond"],
+	possibleGemValue: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+	currentAns: null,
 
-var wins = 0;
-var losses = 0;
-var addition = 0;
-var total = Math.floor((Math.random()* 100) + 19);
-var red = Math.floor((Math.random()* 12) + 1);
-var blue = Math.floor((Math.random()* 12) + 1);
-var yellow = Math.floor((Math.random()* 12) + 1);
-var green = Math.floor((Math.random()* 12) + 1);
+// generates gem value
+	genGemValue: function() {
+		return this.possibleGemValue[Math.floor(Math.random() * this.possibleGemValue.length)];
+	},
 
-var updateAddition = function (){
-	$('.addition').empty();
-	$('.addition').append(addition);
-	$('#wins').empty();
-	$('#wins').append(wins);
-	$('#losses').empty();
-	$('#losses').append(losses);
-}
-var restart = function(){
-	addition = 0;
-    total = Math.floor((Math.random()* 100) + 19);
+	getRandomInt: function() {
+    		return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	},
+// creates game 
+	newGame: function() {
+		this.currentAns = this.getRandomInt();
+		console.log("Current answer: " + this.currentAns);
 
-	$('.total').empty();
-	$('.total').append(total);
+		$("#currentScore").html(this.currentAns);
 
-	red = Math.floor((Math.random()* 12) + 1);
-    blue = Math.floor((Math.random()* 12) + 1);
-    yellow = Math.floor((Math.random()* 12) + 1);
-    green = Math.floor((Math.random()* 12) + 1);
-    updateAddition();
-}
-var logic = function (){
-	if (addition == total) {
-		wins = wins + 1;
-		
-		audio.play();
-		restart();
-	}else if(addition > total){
-		losses = losses + 1;
-		
-		audio.play();
-		restart();
-	}else{
-		updateAddition();
+		this.rubyValue = this.genGemValue();
+		this.emeraldValue = this.genGemValue();
+		this.sapphireValue = this.genGemValue();
+		this.diamondValue = this.genGemValue();
+		console.log("Ruby value: " + this.rubyValue);
+		console.log("Emerald value: " + this.emeraldValue);
+		console.log("Sapphire value: " + this.sapphireValue);
+		console.log("Diamond value: " + this.diamondValue);
+
+		$("#currentUserScore").html(0);
+		this.userScore = 0;
+	},
+
+	loseGame: function() {
+		$("#annouceDisplay").html("You lost!");
+
+// resets to a new game
+		this.newGame();
 	}
 }
-$('.total').append(total);
 
-$('.addition').append(addition);
 
-$(document).ready(function(){
-	$('#red').click(function(){
-		addition = addition + red;
-		logic();
-	})
-	$('#blue').click(function(){
-		addition = addition + blue;
-		logic();
-	})
-	$('#yellow').click(function(){
-		addition = addition + yellow;
-		logic();
-	})
-	$('#green').click(function(){
-		addition = addition + green;
-		logic();
-	})
+// on click functions that add crystal values =========================
+
+
+$("#ruby").on("click", function() {
+	// add crystal value to user score
+	game.userScore += game.rubyValue;
+	// update user score on screen
+	$("#currentUserScore").html(game.userScore);
+	console.log("User score is: " + game.userScore);
+	if (game.userScore === game.currentAns) {
+		$("#annouceDisplay").html("You won!");
+		game.winsCount++
+		$("#winsCountDisplay").html(game.winsCount);
+		game.newGame()
+	} else if (game.userScore > game.currentAns) {
+		$("#annouceDisplay").html("You lost!");
+		game.losesCount++
+		$("#losesCountDisplay").html(game.losesCount);
+		game.newGame()
+	};
 });
 
+$("#emerald").on("click", function() {
+	// add crystal value to user score
+	game.userScore += game.emeraldValue;
+	// update user score on screen
+	$("#currentUserScore").html(game.userScore);
+	console.log("User score is: " + game.userScore);
+	if (game.userScore === game.currentAns) {
+		$("#annouceDisplay").html("You won!");
+		game.winsCount++
+		$("#winsCountDisplay").html(game.winsCount);
+		game.newGame()
+	} else if (game.userScore > game.currentAns) {
+		$("#annouceDisplay").html("You lost!");
+		game.losesCount++
+		$("#losesCountDisplay").html(game.losesCount);
+		game.newGame()
+	};
+});
+
+$("#sapphire").on("click", function() {
+	// add crystal value to user score
+	game.userScore += game.sapphireValue;
+	// update user score on screen
+	$("#currentUserScore").html(game.userScore);
+	console.log("User score is: " + game.userScore);
+	if (game.userScore === game.currentAns) {
+		$("#annouceDisplay").html("You won!");
+		game.winsCount++
+		$("#winsCountDisplay").html(game.winsCount);
+		game.newGame()
+	} else if (game.userScore > game.currentAns) {
+		$("#annouceDisplay").html("You lost!");
+		game.losesCount++
+		$("#losesCountDisplay").html(game.losesCount);
+		game.newGame()
+	};
+});
+
+$("#diamond").on("click", function() {
+	// add crystal value to user score
+	game.userScore += game.diamondValue;
+	// update user score on screen
+	$("#currentUserScore").html(game.userScore);
+	console.log("User score is: " + game.userScore);
+	if (game.userScore === game.currentAns) {
+		$("#annouceDisplay").html("You won!");
+		game.winsCount++
+		$("#winsCountDisplay").html(game.winsCount);
+		game.newGame()
+	} else if (game.userScore > game.currentAns) {
+		$("#annouceDisplay").html("You lost!");
+		game.losesCount++
+		$("#losesCountDisplay").html(game.losesCount);
+		game.newGame()
+	};
+});
+
+// calling new game function
+
+game.newGame();
